620d25153e76e9538cd452274697e5842496c976
Bheemesh A V <bheemeshav438@gmail.com>
2022-01-23 22:04:30 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{ useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = () =>{
+    const [result, setResult] = useState("");
+
+    const handClick = (e) =>{
+            setResult(result.concat(e.target.name));
+    }
+    const clear = () =>{
+        setResult("");
+    }
+    const backspace = () =>{
+        setResult(result.slice(0,-1));
+    }
+    const claculate = () => {
+        try{
+            setResult(eval(result).toString());
+        }catch{
+            setResult("Error");
+        }
+       
+    }
+
+    return (
+        <>
+            <div className='container'>
+                <form>
+                    <input type="text" value={result}/>
+                </form>
+                <div className='keypad'>
+                    <button className='heighlight' onClick={clear} id='clear'>Clear</button>
+                    <button className='heighlight' onClick={backspace} id='backspace'>C</button>
+                    <button className='heighlight' name='/' onClick={handClick}>&divide;</button>
+                    <button name='7' onClick={handClick}>7</button>
+                    <button name='8' onClick={handClick}>8</button>
+                    <button name='9' onClick={handClick}>9</button>
+                    <button className='heighlight' name='*' onClick={handClick}>&times;</button>
+                    <button name='4' onClick={handClick}>4</button>
+                    <button name='5' onClick={handClick}>5</button>
+                    <button name='6' onClick={handClick}>6</button>
+                    <button className='heighlight' name='-' onClick={handClick}>&ndash;</button>
+                    <button name='1' onClick={handClick}>1</button>
+                    <button name='2' onClick={handClick}>2</button>
+                    <button name='3' onClick={handClick}>3</button>
+                    <button className='heighlight' name='+' onClick={handClick}>+</button>
+                    <button name='0' onClick={handClick}>0</button>
+                    <button name='.' onClick={handClick}>.</button>
+                    <button className='heighlight' onClick={claculate} id='result'>=</button>
+                </div>
+            </div>
+        </>
+    );
 }
 
 export default App;
